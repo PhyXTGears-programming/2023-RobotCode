b1d6c12a0ae406067908ce53433a4e065ca7c38f
PhyXTGears <phyxtgearspro@gmail.com>
2023-01-31 18:51:54 -0500

    Add .0 to arm consts to denote float type
    hEehOO Add tHe .0
    - Emmie Jones

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -2,5 +2,5 @@
 #include "Interfaces.h"
 
 //            ARM PARAMS:
-const float k_forearmLenInches = 24; // Update to correct value
-const float k_bicepLenInches = 24;// Update to correct value
+const float k_forearmLenInches = 24.0; // Update to correct value
+const float k_bicepLenInches = 24.0;// Update to correct value
