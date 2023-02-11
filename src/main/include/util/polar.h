5fd4cefbb9783310d9130444ad4b0369d996055b
nickquednow <nickquednow@gmail.com>
2023-02-10 22:32:21 -0500

    bugfix: make sure magnitude is initialized in polar constructor

--- src/main/include/util/polar.h
+++ src/main/include/util/polar.h
@@ -2,7 +2,7 @@
 
 class Polar {
     public:
-        Polar(float radian, float radius) : radian(radian), magnitude(magnitude) {}
+        Polar(float radian, float magnitude) : radian(radian), magnitude(magnitude) {}
         Polar(){ // default constructor
             radian = 0;
             magnitude = 0;
