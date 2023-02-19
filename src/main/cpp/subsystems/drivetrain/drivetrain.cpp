a9db41fd96b3d7239e3beaa0a251267f0860b08d
nickquednow <56178732+nickquednow@users.noreply.github.com>
2023-02-19 01:05:00 -0500

    remove debugging lines
    accidentally had debugging lines in the commit, removed so it will compile.

--- src/main/cpp/subsystems/drivetrain/drivetrain.cpp
+++ src/main/cpp/subsystems/drivetrain/drivetrain.cpp
@@ -47,8 +47,6 @@ void Drivetrain::Periodic(){
     if(m_fieldOriented){
         //gets the current angle of the NavX (reported in degrees, converted to radians)
         m_fieldOrientedOffset = DEGREES_TO_RADIANS(m_navX->GetYaw())+M_PI;
-        gyro->PutNumber("yaw",m_fieldOrientedOffset);
-        
     }
     //just tell the motor abstractions to pet the watchdog and update the motors
     for (int i = 0; i < Constants::k_NumberOfSwerveModules; i++) {
