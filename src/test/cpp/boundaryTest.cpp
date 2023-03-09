4d104f3543981d6ab54d27c3651a428b05167210
PhyXTGears <phyxtgearspro@gmail.com>
2023-03-08 19:26:07 -0500

    test: Add tests for box and sphere boundaries
    - Justin C

--- src/test/cpp/boundaryTest.cpp
+++ src/test/cpp/boundaryTest.cpp
@@ -3,6 +3,32 @@
 #include "util/point.h"
 #include "subsystems/arm/boundary.h"
 
+TEST(BoxBoundaryTest, GivenPointInsideBox_IsInsideReturnsTrue) {
+    BoxBoundary boundary {
+        -1.0, 1.0,
+        -1.0, 1.0,
+        -1.0, 1.0
+    };
+
+    Point insidePoint {0.0, 0.0, 0.0};
+
+    ASSERT_TRUE(boundary.isInside(insidePoint));
+    ASSERT_FALSE(boundary.isOutside(insidePoint));
+}
+
+TEST(BoxBoundaryTest, GivenPointOutsideBox_IsInsideReturnsFalse) {
+    BoxBoundary boundary {
+        -1.0, 1.0,
+        -1.0, 1.0,
+        -1.0, 1.0
+    };
+
+    Point outsidePoint {2.0, 0.0, 0.0};
+
+    ASSERT_FALSE(boundary.isInside(outsidePoint));
+    ASSERT_TRUE(boundary.isOutside(outsidePoint));
+}
+
 TEST(ConvexPolygonBoundaryTest, GivenPointInsideTriangle_IsInsideReturnsTrue) {
     ConvexPolygonBoundary boundary {
         {
@@ -33,4 +59,28 @@ TEST(ConvexPolygonBoundaryTest, GivenPointOutsideTriangle_IsInsideReturnsFalse) 
 
     ASSERT_FALSE(boundary.isInside(inTrianglePoint));
     ASSERT_TRUE(boundary.isOutside(inTrianglePoint));
+}
+
+TEST(SphereBoundaryTest, GivenPointInsideSphere_IsInsideReturnsTrue) {
+    SphereBoundary boundary {
+        Point{0.0, 0.0, 0.0},
+        1.0
+    };
+
+    Point insidePoint {0.0, 0.0, 0.5};
+
+    ASSERT_TRUE(boundary.isInside(insidePoint));
+    ASSERT_FALSE(boundary.isOutside(insidePoint));
+}
+
+TEST(SphereBoundaryTest, GivenPointOutsideSphere_IsInsideReturnsFalse) {
+    SphereBoundary boundary {
+        Point{0.0, 0.0, 0.0},
+        1.0
+    };
+
+    Point outsidePoint {2.0, 0.0, 0.0};
+
+    ASSERT_FALSE(boundary.isInside(outsidePoint));
+    ASSERT_TRUE(boundary.isOutside(outsidePoint));
 }
