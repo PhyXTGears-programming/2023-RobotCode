8bab8785b746da2296f62d58cbffec03e764f9e4
PhyXTGears <phyxtgearspro@gmail.com>
2023-03-11 15:30:11 -0500

    Report field oriented mode on dashboard
    - Nick Q

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -18,6 +18,8 @@
 
 #include "external/cpptoml.h"
 
+#include <frc/smartdashboard/SmartDashboard.h>
+
 #ifdef COMPETITION_MODE
 #include <frc2/command/SequentialCommandGroup.h>
 #include <frc2/command/Command.h>
@@ -78,6 +80,8 @@ void Robot::RobotInit() {
  */
 void Robot::RobotPeriodic() {
   frc2::CommandScheduler::GetInstance().Run();
+
+  frc::SmartDashboard::PutBoolean("Field Centric Enabled",c_drivetrain->getFieldCentric());
 }
 
 /**
