e7cf4b6e5643718da1fb85115db695e962ff9f67
PhyXTGears <phyxtgearspro@gmail.com>
2023-03-12 13:44:35 -0400

    drive: Increase teleop speed for eliminations rounds
    - Justin C and Nick Q

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -4,9 +4,9 @@
 namespace Constants {
     const int k_NumberOfSwerveModules = 4;
 
-    const double k_minDriveSpeed = 0.15;
-    const double k_normalDriveSpeed = 0.30;
-    const double k_maxDriveSpeed = 0.60;
+    const double k_minDriveSpeed = 0.25;
+    const double k_normalDriveSpeed = 0.50;
+    const double k_maxDriveSpeed = 0.70;
     const double k_maxSpinSpeed = 1.00;
 
     namespace Arm {
